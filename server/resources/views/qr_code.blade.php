d4eacccce0fe4392f6df5b46f18dba0031a36e2c
atras30 <atrasshalhan@gmail.com>
2022-10-26 14:16:07 +0700

    qr code on progress

---
+++ server/resources/views/qr_code.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Qr Code</title>
+</head>
+
+<body>
+  {!! Storage::disk('local')->put('example.txt', QrCode::generate('Make me into a QrCode!')) !!}
+</body>
+
+</html>
