62530bf52a0d992bd5f76eb86819b0264c43a26b
Jonathan Putra <jonathan.putra@student.umn.ac.id>
2022-11-03 10:21:56 +0700

    Finalisasi Search UNTUK UI

--- app/app/src/main/java/umn/ac/id/project/maggot/BuyMaggotActivity.java
+++ app/app/src/main/java/umn/ac/id/project/maggot/BuyMaggotActivity.java
@@ -11,13 +11,18 @@ import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -43,6 +48,7 @@ import umn.ac.id.project.maggot.retrofit.ApiService;
 public class BuyMaggotActivity extends AppCompatActivity {
     TextView dateTimeDisplay;
     private Calendar calendar;
+    EditText searchPeternak;
     private SimpleDateFormat dateFormat;
     private String date;
     List<PeternakModel.Peternak> results;
@@ -55,11 +61,11 @@ public class BuyMaggotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_maggot);
-        Button qrCodeScannerButton = findViewById(R.id.scan_qr_code_button);
-
-        qrCodeScannerButton.setOnClickListener(v -> {
-            scanCode();
-        });
+//        Button qrCodeScannerButton = findViewById(R.id.scan_qr_code_button);
+//
+//        qrCodeScannerButton.setOnClickListener(v -> {
+//            scanCode();
+//        });
 
         ApiService.endpoint().getPeternak().enqueue(new Callback<PeternakModel>() {
             @Override
@@ -71,33 +77,67 @@ public class BuyMaggotActivity extends AppCompatActivity {
                         name[i] = results.get(i).getFull_name();
                     }
                     nameAdapter = new ArrayAdapter<String>(BuyMaggotActivity.this, android.R.layout.simple_list_item_1, name);
-                    Spinner spinner = (Spinner) findViewById(R.id.listPeternakSpinner);
-                    // Create an ArrayAdapter using the string array and a default spinner layout
-                    adapter = new DropDownAdapter(BuyMaggotActivity.this, R.layout.peternak_dropdown, R.id.namaPeternak, results);
-                    // Specify the layout to use when the list of choices appears
-                    // Apply the adapter to the spinner
-                    spinner.setAdapter(adapter);
-                    listView = findViewById(R.id.listview);
-                    listView.setAdapter(nameAdapter);
-                    listView.setVisibility(View.VISIBLE);
-                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    umn.ac.id.project.maggot.InstantAutoComplete textView = (umn.ac.id.project.maggot.InstantAutoComplete) findViewById(R.id.inputpeternak);
+                    textView.setAdapter(nameAdapter);
+
+                    textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
                         @Override
-                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            selectedFromList = (String) (listView.getItemAtPosition(i));
-                            Toast.makeText(BuyMaggotActivity.this, "You clicked" + selectedFromList, Toast.LENGTH_SHORT).show();
-                            listView.setVisibility(View.GONE);
+                        public void onFocusChange(View v, boolean hasFocus) {
+                            if (hasFocus)
+                                textView.showDropDown();
 
-
-                            for(PeternakModel.Peternak farmer : results)
-                            {
-                                if(farmer.getFull_name().equals(selectedFromList))
-                                {
-                                    int spinnerPosition = adapter.getPosition(farmer);
-                                    spinner.setSelection(spinnerPosition);
-                                }
-                            }
                         }
                     });
+
+                    textView.setOnTouchListener(new View.OnTouchListener() {
+
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            textView.showDropDown();
+                            return false;
+                        }
+                    });
+
+//                    searchPeternak = findViewById((R.id.searchpeternak));
+//                    searchPeternak.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//                        @Override
+//                        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+//                                performSearch();
+//                                return true;
+//                            }
+//                            return false;
+//                        }
+//                    });
+
+//                    Spinner spinner = (Spinner) findViewById(R.id.listPeternakSpinner);
+//                    // Create an ArrayAdapter using the string array and a default spinner layout
+//                    adapter = new DropDownAdapter(BuyMaggotActivity.this, R.layout.peternak_dropdown, R.id.namaPeternak, results);
+//                    // Specify the layout to use when the list of choices appears
+//                    // Apply the adapter to the spinner
+//                    spinner.setAdapter(adapter);
+//                    listView = findViewById(R.id.listview);
+//                    listView.setAdapter(nameAdapter);
+//                    listView.setVisibility(View.VISIBLE);
+//                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                        @Override
+//                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                            selectedFromList = (String) (listView.getItemAtPosition(i));
+//                            Toast.makeText(BuyMaggotActivity.this, "You clicked" + selectedFromList, Toast.LENGTH_SHORT).show();
+//                            listView.setVisibility(View.GONE);
+//
+//
+//                            for(PeternakModel.Peternak farmer : results)
+//                            {
+//                                if(farmer.getFull_name().equals(selectedFromList))
+//                                {
+//                                    int spinnerPosition = adapter.getPosition(farmer);
+//                                    spinner.setSelection(spinnerPosition);
+//                                }
+//                            }
+//                        }
+//                    });
 
 
 
