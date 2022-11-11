d4526fa5b2b345dfcfa7ce9e9b3f6c5ceb8cd3ae
Adrian Finantyo <adrianfinantyo@gmail.com>
2022-11-11 14:35:45 +0700

    add: dashboard layout

--- web/src/AppRouter.tsx
+++ web/src/AppRouter.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import App from "./App";
+import DashboardLayout from "./layout/DashboardLayout";
 import Login from "./pages/Login";
 
 const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<DashboardLayout />} />
       <Route path="/" element={<App />} />
     </Routes>
   );
---
+++ web/src/layout/DashboardLayout.tsx
@@ -0,0 +1,57 @@
+import { Heading, Box, VStack, Button, Divider, Flex, Input, Image, Text, Icon, Center, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./DashboardLayout.scss";
+import { FaHome, FaSearch } from "react-icons/fa";
+
+const NavButton = (props: any) => {
+  return (
+    <NavLink to={props.to} className="dash-link">
+      <Flex w="100%" className="container" alignItems="center" gap="1rem">
+        <Center boxSize="3rem" className="icon">
+          <Icon as={FaHome} boxSize="1.2rem" />
+        </Center>
+        <Text fontWeight="bold" fontSize="md">
+          {props.children}
+        </Text>
+      </Flex>
+    </NavLink>
+  );
+};
+
+const DashboardLayout: React.FC = () => {
+  return (
+    <Flex bgColor="var(--color-light)" minH="100vh" w="100%" p="2rem" className="dashboard">
+      <Flex as="aside" flexDir="column" w="20rem" pos="fixed" gap="0.5rem">
+        <Flex alignItems="center" gap="0.5rem" justifyContent="center">
+          <Image src="/assets/MagFinance_logo.png" boxSize="50px" />
+          <Heading fontSize="2xl" color="var(--color-secondary)">
+            MagFinance
+          </Heading>
+        </Flex>
+        <Divider h="2px" bgColor="var(--color-primary)" my="1rem" />
+        <NavButton to="/">Overview</NavButton>
+        <NavButton to="/dashboard">Pengelola Bank Sampah</NavButton>
+        <NavButton to="/">Warga</NavButton>
+        <NavButton to="/">Warung</NavButton>
+      </Flex>
+      <VStack w="100%" ml="22rem" alignItems="flex-start" gap="1rem">
+        <Heading>Dashboard</Heading>
+        <Box w="100%" h="15rem" bgColor="var(--color-white)" borderRadius="15px" p="2rem">
+          <Flex justifyContent="space-between" alignItems="center">
+            <Heading fontSize="xl">Daftar Pengelola Bank Sampah</Heading>
+            <InputGroup w="20rem">
+              <InputLeftElement pointerEvents="none" children={<Icon as={FaSearch} color="gray.300" />} />
+              <Input placeholder="Cari nama pengelola bank sampah" borderRadius="30px" />
+            </InputGroup>
+          </Flex>
+        </Box>
+        <Text as="footer">
+          @ 2022, Made with ❤️️ by <b>Adrian Fiantnyo, Atras Salhaan, Reynard Yaputra</b> & <b>Jonathan Putra</b>.
+        </Text>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default DashboardLayout;
