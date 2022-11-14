97a0f6d754e16c2b5f0787bbfe079d6212551780
GenshinimpactJokijo <jimmywangkar@gmail.com>
2022-11-14 22:44:51 +0700

    Penambahan & perbaikan fitur search pada buy maggot

--- app/app/src/main/java/umn/ac/id/project/maggot/BuyMaggotActivity2.java
+++ app/app/src/main/java/umn/ac/id/project/maggot/BuyMaggotActivity2.java
@@ -3,12 +3,75 @@ package umn.ac.id.project.maggot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import umn.ac.id.project.maggot.model.PeternakModel;
+import umn.ac.id.project.maggot.retrofit.ApiService;
 
 public class BuyMaggotActivity2 extends AppCompatActivity {
-
+    ArrayAdapter<String> nameAdapter;
+    List<PeternakModel.Peternak> results;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_maggot2);
+        ApiService.endpoint().getPeternak().enqueue(new Callback<PeternakModel>() {
+            @Override
+            public void onResponse(Call<PeternakModel> call, Response<PeternakModel> response) {
+                if(response.isSuccessful()) {
+                    results = response.body().getPeternak();
+                    String name[] = new String[results.size()];
+                    for (int i=0; i<results.size(); i++) {
+                        name[i] = results.get(i).getFull_name();
+                    }
+                    nameAdapter = new ArrayAdapter<String>(BuyMaggotActivity2.this, android.R.layout.simple_list_item_1, name);
+                    umn.ac.id.project.maggot.InstantAutoComplete textView = (umn.ac.id.project.maggot.InstantAutoComplete) findViewById(R.id.namawarga);
+                    textView.setAdapter(nameAdapter);
+
+                    textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+                        @Override
+                        public void onFocusChange(View v, boolean hasFocus) {
+                            if (hasFocus) {
+                                textView.setText("");
+                                textView.showDropDown();
+                            } else {
+                                textView.showDropDown();
+                                textView.setText("Masukkan nama warga disini...");
+
+                            }
+                        }
+                    });
+
+                    textView.setOnTouchListener(new View.OnTouchListener() {
+
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            textView.showDropDown();
+                            return false;
+                        }
+                    });
+
+//
+
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PeternakModel> call, Throwable t) {
+                Log.d("Failure : ", t.toString());
+            }
+        });
     }
+
 }
