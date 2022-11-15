c43a43c8b3848735d41fc68a44f97ded143af34a
GenshinimpactJokijo <jimmywangkar@gmail.com>
2022-11-15 11:31:57 +0700

    menambahkan fitur search pada fragment farmer payment

--- app/app/src/main/java/umn/ac/id/project/maggot/FarmerPaymentFragment.java
+++ app/app/src/main/java/umn/ac/id/project/maggot/FarmerPaymentFragment.java
@@ -6,19 +6,34 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import umn.ac.id.project.maggot.model.PeternakModel;
+import umn.ac.id.project.maggot.model.WarungModel;
+import umn.ac.id.project.maggot.retrofit.ApiService;
+
 public class FarmerPaymentFragment extends Fragment {
     private Context context;
-
+    ArrayAdapter<String> nameAdapter;
+    List<WarungModel.Warung> results;
     //QR Scanner
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
         if(result.getContents() != null) {
@@ -46,14 +61,61 @@ public class FarmerPaymentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_farmer_payment, container, false);
-
         //QR Code scanner button logic
         MaterialButton scanQrCodeButton = view.findViewById(R.id.scan_qr_code_button);
         scanQrCodeButton.setOnClickListener(v -> {
             //Open QR Scanner activity
             scanCode();
         });
+        ApiService.endpoint().getWarung().enqueue(new Callback<WarungModel>() {
+            @Override
+            public void onResponse(@NonNull Call<WarungModel> call, @NonNull Response<WarungModel> response) {
+                if(response.isSuccessful()) {
+                    results = response.body().getWarung();
+                    String name[] = new String[results.size()];
+                    for (int i=0; i<results.size(); i++) {
+                        name[i] = results.get(i).getFull_name();
+                    }
+                    nameAdapter = new ArrayAdapter<String>(context , android.R.layout.simple_list_item_1, name);
+                    umn.ac.id.project.maggot.InstantAutoComplete textView = (umn.ac.id.project.maggot.InstantAutoComplete) view.findViewById(R.id.namawarung);
+                    textView.setAdapter(nameAdapter);
 
+                    textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+                        @Override
+                        public void onFocusChange(View v, boolean hasFocus) {
+                            if (hasFocus) {
+                                textView.setText("");
+                                textView.showDropDown();
+                            } else {
+                                textView.showDropDown();
+                                textView.setText("Masukkan nama warung disini...");
+
+                            }
+                        }
+                    });
+
+                    textView.setOnTouchListener(new View.OnTouchListener() {
+
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            textView.showDropDown();
+                            return false;
+                        }
+                    });
+
+//
+
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WarungModel> call, Throwable t) {
+                Log.d("Failure : ", t.toString());
+            }
+        });
         // Inflate the layout for this fragment
         return view;
     }
