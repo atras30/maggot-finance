4c47056cf1dc0981e431adb2f855cea520af1cb7
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2022-11-17 08:58:22 +0700

    Email Minor Change

--- server/resources/views/mail/user_registration.blade.php
+++ server/resources/views/mail/user_registration.blade.php
@@ -1,5 +1,5 @@
 <p>Halo, {{$createdUser->full_name}}!</p>
-<p>Anda telah mengajukan pembuatan akun warga ke pengelola bank sampah {{$createdUser->trash_manager->tempat}}. Mohon untuk menunggu selama 1 sampai 3 hari jam kerja untuk proses pengajuan ini. Jika proses pengajuan melebihi waktu 3 hari kerja, maka silakan hubungi pengelola bank sampah {{$trashManager->tempat}} di email berikut: {{$trashManager->email}} untuk informasi lebih lanjut.</p>
+<p>Anda telah mengajukan pembuatan akun warga ke pengelola bank sampah {{$createdUser->trash_manager->tempat}}. Mohon untuk menunggu selama 1 sampai 3 hari kerja untuk proses pengajuan ini. Jika proses pengajuan melebihi waktu 3 hari kerja, maka silakan hubungi pengelola bank sampah {{$trashManager->tempat}} di email berikut: {{$trashManager->email}} untuk informasi lebih lanjut.</p>
 <p>Terima kasih telah mendaftarkan diri pada aplikasi MagFinance!</p>
 <p>Salam hangat,</p>
 <p>Tim MagFinance</p>
