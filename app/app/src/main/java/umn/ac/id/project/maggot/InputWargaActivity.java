dbcda74d784a2492307fc0fffd1dc988d9145f99
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2022-11-19 09:06:54 +0700

    Input Warga & Warung Layout

---
+++ app/app/src/main/java/umn/ac/id/project/maggot/InputWargaActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.project.maggot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputWargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_warga);
+    }
+}
